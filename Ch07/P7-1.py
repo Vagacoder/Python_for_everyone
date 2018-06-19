c9c48bcf7287c7bab80f66d031026fc2e768a291
Qirui Hu <qhu@sbcc.local>
2018-06-18 18:53:35 -0700

    add files

--- Ch07/P7-1.py
+++ Ch07/P7-1.py
@@ -2,9 +2,13 @@
 ##Ch07 P7.1
 
 outfile = open('hello.txt', 'w')
-outfile.write('Hello, World!')
+outfile.write('Hello, World!\n')
+print("This is done using print command.", file = outfile)
 outfile.close()
 
 infile = open('Hello.txt', 'r')
 text = infile.readline()
 print(text)
+text = infile.readline()
+print(text)
+infile.close()
---
+++ test3.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+str1 = "a b c d e f"
+print(str1.rsplit(" ", 2))
